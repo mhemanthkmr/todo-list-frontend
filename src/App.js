1b7ee8bce4de0300d76fd2cc95fc46f22bb77131
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-08-02 17:10:18 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:5000/api/todos"; // Replace with your backend API URL
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodoText, setNewTodoText] = useState("");
+
+  // Fetch all todos from the backend
+  useEffect(() => {
+    axios
+      .get(API_BASE_URL)
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+      });
+  }, []);
+
+  // Add a new todo
+  const addTodo = () => {
+    if (!newTodoText.trim()) return;
+
+    axios
+      .post(API_BASE_URL, { text: newTodoText })
+      .then((response) => {
+        setTodos([...todos, response.data]);
+        setNewTodoText("");
+      })
+      .catch((error) => {
+        console.error("Error adding todo:", error);
+      });
+  };
+
+  // Delete a todo
+  const deleteTodo = (id) => {
+    axios
+      .delete(`${API_BASE_URL}/${id}`)
+      .then(() => {
+        setTodos(todos.filter((todo) => todo._id !== id));
+      })
+      .catch((error) => {
+        console.error("Error deleting todo:", error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
