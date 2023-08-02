5b98a3b3e7604cb28a0af2447db2cc4f8d108fbf
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-08-02 17:34:04 +0000

    Running Successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Container, Row, Col, Form, Button, ListGroup } from "react-bootstrap";
+import "./App.css";
 
-const API_BASE_URL = "http://localhost:5000/api/todos"; // Replace with your backend API URL
+const API_BASE_URL = "http://localhost:5000/api/todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodoText, setNewTodoText] = useState("");
 
-  // Fetch all todos from the backend
   useEffect(() => {
     axios
       .get(API_BASE_URL)
@@ -19,10 +20,8 @@ function App() {
       });
   }, []);
 
-  // Add a new todo
   const addTodo = () => {
     if (!newTodoText.trim()) return;
-
     axios
       .post(API_BASE_URL, { text: newTodoText })
       .then((response) => {
@@ -34,7 +33,6 @@ function App() {
       });
   };
 
-  // Delete a todo
   const deleteTodo = (id) => {
     axios
       .delete(`${API_BASE_URL}/${id}`)
@@ -47,25 +45,49 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <div>
-        <input
-          type="text"
-          value={newTodoText}
-          onChange={(e) => setNewTodoText(e.target.value)}
-        />
-        <button onClick={addTodo}>Add Todo</button>
-      </div>
-      <ul>
+    <Container className="mt-5">
+      <h1 className="mb-4">Todo List</h1>
+      <Form className="mb-3">
+        <Row>
+          <Col xs={9}>
+            <Form.Control
+              type="text"
+              value={newTodoText}
+              onChange={(e) => setNewTodoText(e.target.value)}
+              placeholder="Enter a new todo"
+            />
+          </Col>
+          <Col xs={3}>
+            <Button
+              type="submit"
+              onClick={addTodo}
+              variant="primary"
+              block
+              disabled={!newTodoText.trim()}
+            >
+              Add Todo
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+      <ListGroup>
         {todos.map((todo) => (
-          <li key={todo._id}>
+          <ListGroup.Item
+            key={todo._id}
+            className="d-flex justify-content-between align-items-center"
+          >
             {todo.text}
-            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
-          </li>
+            <Button
+              onClick={() => deleteTodo(todo._id)}
+              variant="danger"
+              size="sm"
+            >
+              Delete
+            </Button>
+          </ListGroup.Item>
         ))}
-      </ul>
-    </div>
+      </ListGroup>
+    </Container>
   );
 }
 
