7279e3776dcba5b8b5fd72ea64066ba5cb6060a9
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-08-04 13:51:30 +0000

    Resolved Bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Container, Row, Col, Form, Button, ListGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  ListGroup,
+  Alert,
+} from "react-bootstrap";
 import "./App.css";
 
 const API_BASE_URL = "https://node.mhemanthkmr.live/api/todos/";
@@ -8,6 +16,8 @@ const API_BASE_URL = "https://node.mhemanthkmr.live/api/todos/";
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodoText, setNewTodoText] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [variant, setVariant] = useState("");
 
   useEffect(() => {
     axios
@@ -22,14 +32,19 @@ function App() {
 
   const addTodo = () => {
     if (!newTodoText.trim()) return;
+
     axios
       .post(API_BASE_URL, { text: newTodoText })
       .then((response) => {
         setTodos([...todos, response.data]);
         setNewTodoText("");
+        setAlertMessage("Todo added successfully");
+        setVariant("success");
       })
       .catch((error) => {
         console.error("Error adding todo:", error);
+        setAlertMessage("Error adding todo");
+        setVariant("danger");
       });
   };
 
@@ -38,9 +53,13 @@ function App() {
       .delete(`${API_BASE_URL}/${id}`)
       .then(() => {
         setTodos(todos.filter((todo) => todo._id !== id));
+        setAlertMessage("Todo deleted successfully");
+        setVariant("success");
       })
       .catch((error) => {
         console.error("Error deleting todo:", error);
+        setAlertMessage("Error deleting todo");
+        setVariant("danger");
       });
   };
 
@@ -56,9 +75,13 @@ function App() {
             todo._id === updatedTodo._id ? updatedTodo : todo
           )
         );
+        setAlertMessage("Todo updated successfully");
+        setVariant("success");
       })
       .catch((error) => {
         console.error("Error updating todo:", error);
+        setAlertMessage("Error updating todo");
+        setVariant("danger");
       });
   };
 
@@ -80,13 +103,17 @@ function App() {
               onClick={addTodo}
               variant="primary"
               disabled={!newTodoText.trim()}
-              type="submit"
             >
               Add Todo
             </Button>
           </Col>
         </Row>
       </Form>
+      {alertMessage && (
+        <Alert variant={variant} className="mb-3" dismissible>
+          {alertMessage}
+        </Alert>
+      )}
       <ListGroup>
         {todos.map((todo) => (
           <ListGroup.Item
