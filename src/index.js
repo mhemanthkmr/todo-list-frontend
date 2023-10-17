77e6c721f8a785bb743d5b5d39361f397d9d9b15
HemanthKumar M <m.hemanthkmr143@gmail.com>
2023-10-17 18:09:05 +0000

    Installed a Browder Routers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const token = localStorage.getItem("token");
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
